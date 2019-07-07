5b1b5d365b846edf8cf453564b1d01e2adf8d683
severjason <ssever2010@gmail.com>
2019-07-07 22:18:38 +0300

    added footer

---
+++ src/app/footer/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: theme.spacing(6),
+    backgroundColor: theme.palette.primary.dark,
+  },
+  text: {
+    color: theme.palette.primary.contrastText,
+  }
+}));
+
+const Footer: React.FC<InjectedIntlProps> = ({intl}) => {
+
+  const classes = useStyles();
+
+  const fullName = `${intl.messages['cv.firstName']} ${intl.messages['cv.lastName']}`;
+
+  return (
+    <Grid container={true} className={classes.root} justify="center" alignItems="center">
+      <Typography className={classes.text}>
+        {`${fullName} @ ${new Date().getFullYear()}`}
+      </Typography>
+    </Grid>
+  );
+};
+
+export default injectIntl(Footer);
